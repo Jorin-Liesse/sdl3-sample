7f0fb266dbef8b8db373caeae1791b7f377470a2
Jorin-Liesse <liesse.jorin@gmail.com>
2025-09-04 14:48:33 +0200

    Enhance CMakeLists.txt to automatically include all .cpp files in src/ and streamline source management in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,73 +3,91 @@
 constexpr uint32_t windowStartWidth = 400;
 constexpr uint32_t windowStartHeight = 400;
 
-struct AppContext {
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-    SDL_Texture* messageTex, *imageTex;
+struct AppContext
+{
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Texture *messageTex, *imageTex;
     SDL_FRect messageDest;
     SDL_AudioDeviceID audioDevice;
-    MIX_Track* track;
+    MIX_Track *track;
     SDL_AppResult app_quit = SDL_APP_CONTINUE;
 };
 
-SDL_AppResult SDL_Fail(){
+SDL_AppResult SDL_Fail()
+{
     SDL_LogError(SDL_LOG_CATEGORY_CUSTOM, "Error %s", SDL_GetError());
     return SDL_APP_FAILURE;
 }
 
-SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+{
+#if __ANDROID__
+    SDL_SetHint(SDL_HINT_ANDROID_BLOCK_ON_PAUSE, "1");
+    SDL_SetHint(SDL_HINT_ANDROID_TRAP_BACK_BUTTON, "1");
+    SDL_SetHint(SDL_HINT_VIDEO_ANDROID_WINDOW_FULLSCREEN, "1");
+#endif
+
     // init the library, here we make a window so we only need the Video capabilities.
-    if (not SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)){
+    if (not SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO))
+    {
         return SDL_Fail();
     }
-    
+
     // init TTF
-    if (not TTF_Init()) {
+    if (not TTF_Init())
+    {
         return SDL_Fail();
     }
-    
+
     // init Mixer
-    if (not MIX_Init()) {
+    if (not MIX_Init())
+    {
         return SDL_Fail();
     }
-    
+
     // create a window
-   
-    SDL_Window* window = SDL_CreateWindow("SDL Minimal Sample", windowStartWidth, windowStartHeight, SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY);
-    if (not window){
+
+    SDL_Window *window = SDL_CreateWindow("SDL Minimal Sample", windowStartWidth, windowStartHeight, SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY);
+    if (not window)
+    {
         return SDL_Fail();
     }
-    
+
+    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+
     // create a renderer
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
-    if (not renderer){
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+    if (not renderer)
+    {
         return SDL_Fail();
     }
-    
+
     // load the font
 #if __ANDROID__
-    std::filesystem::path basePath = "";   // on Android we do not want to use basepath. Instead, assets are available at the root directory.
+    std::filesystem::path basePath = ""; // on Android we do not want to use basepath. Instead, assets are available at the root directory.
 #else
     auto basePathPtr = SDL_GetBasePath();
-     if (not basePathPtr){
+    if (not basePathPtr)
+    {
         return SDL_Fail();
     }
-     const std::filesystem::path basePath = basePathPtr;
+    const std::filesystem::path basePath = basePathPtr;
 #endif
 
     const auto fontPath = basePath / "Inter-VariableFont.ttf";
-    TTF_Font* font = TTF_OpenFont(fontPath.string().c_str(), 36);
-    if (not font) {
+    TTF_Font *font = TTF_OpenFont(fontPath.string().c_str(), 36);
+    if (not font)
+    {
         return SDL_Fail();
     }
 
     // render the font to a surface
     const std::string_view text = "Hello SDL!";
-    SDL_Surface* surfaceMessage = TTF_RenderText_Solid(font, text.data(), text.length(), { 255,255,255 });
+    SDL_Surface *surfaceMessage = TTF_RenderText_Solid(font, text.data(), text.length(), {255, 255, 255});
 
     // make a texture from the surface
-    SDL_Texture* messageTex = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+    SDL_Texture *messageTex = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
 
     // we no longer need the font or the surface, so we can destroy those now.
     TTF_CloseFont(font);
@@ -77,38 +95,38 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
 
     // load the SVG
     auto svg_surface = IMG_Load((basePath / "gs_tiger.svg").string().c_str());
-    SDL_Texture* tex = SDL_CreateTextureFromSurface(renderer, svg_surface);
+    SDL_Texture *tex = SDL_CreateTextureFromSurface(renderer, svg_surface);
     SDL_DestroySurface(svg_surface);
-    
 
     // get the on-screen dimensions of the text. this is necessary for rendering it
     auto messageTexProps = SDL_GetTextureProperties(messageTex);
     SDL_FRect text_rect{
-            .x = 0,
-            .y = 0,
-            .w = float(SDL_GetNumberProperty(messageTexProps, SDL_PROP_TEXTURE_WIDTH_NUMBER, 0)),
-            .h = float(SDL_GetNumberProperty(messageTexProps, SDL_PROP_TEXTURE_HEIGHT_NUMBER, 0))
-    };
+        .x = 0,
+        .y = 0,
+        .w = float(SDL_GetNumberProperty(messageTexProps, SDL_PROP_TEXTURE_WIDTH_NUMBER, 0)),
+        .h = float(SDL_GetNumberProperty(messageTexProps, SDL_PROP_TEXTURE_HEIGHT_NUMBER, 0))};
 
     // init SDL Mixer
-    MIX_Mixer* mixer = MIX_CreateMixerDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL);
-    if (mixer == nullptr) {
+    MIX_Mixer *mixer = MIX_CreateMixerDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL);
+    if (mixer == nullptr)
+    {
         return SDL_Fail();
     }
-    
+
     auto mixerTrack = MIX_CreateTrack(mixer);
 
     // load the music
     auto musicPath = basePath / "the_entertainer.ogg";
-    auto music = MIX_LoadAudio(mixer,musicPath.string().c_str(),false);
-    if (not music) {
+    auto music = MIX_LoadAudio(mixer, musicPath.string().c_str(), false);
+    if (not music)
+    {
         return SDL_Fail();
     }
 
     // play the music (does not loop)
     MIX_SetTrackAudio(mixerTrack, music);
     MIX_PlayTrack(mixerTrack, NULL);
-    
+
     // print some information about the window
     SDL_ShowWindow(window);
     {
@@ -117,47 +135,51 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
         SDL_GetWindowSizeInPixels(window, &bbwidth, &bbheight);
         SDL_Log("Window size: %ix%i", width, height);
         SDL_Log("Backbuffer size: %ix%i", bbwidth, bbheight);
-        if (width != bbwidth){
+        if (width != bbwidth)
+        {
             SDL_Log("This is a highdpi environment.");
         }
     }
 
     // set up the application data
     *appstate = new AppContext{
-       .window = window,
-       .renderer = renderer,
-       .messageTex = messageTex,
-       .imageTex = tex,
-       .messageDest = text_rect,
-       .track = mixerTrack,
+        .window = window,
+        .renderer = renderer,
+        .messageTex = messageTex,
+        .imageTex = tex,
+        .messageDest = text_rect,
+        .track = mixerTrack,
     };
-    
-    SDL_SetRenderVSync(renderer, -1);   // enable vysnc
-    
+
+    SDL_SetRenderVSync(renderer, -1); // enable vysnc
+
     SDL_Log("Application started successfully!");
 
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event* event) {
-    auto* app = (AppContext*)appstate;
-    
-    if (event->type == SDL_EVENT_QUIT) {
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    auto *app = (AppContext *)appstate;
+
+    if (event->type == SDL_EVENT_QUIT)
+    {
         app->app_quit = SDL_APP_SUCCESS;
     }
 
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppIterate(void *appstate) {
-    auto* app = (AppContext*)appstate;
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    auto *app = (AppContext *)appstate;
 
     // draw a color
     auto time = SDL_GetTicks() / 1000.f;
     auto red = (std::sin(time) + 1) / 2.0 * 255;
     auto green = (std::sin(time / 2) + 1) / 2.0 * 255;
     auto blue = (std::sin(time) * 2 + 1) / 2.0 * 255;
-    
+
     SDL_SetRenderDrawColor(app->renderer, red, green, blue, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(app->renderer);
 
@@ -170,16 +192,18 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
     return app->app_quit;
 }
 
-void SDL_AppQuit(void* appstate, SDL_AppResult result) {
-    auto* app = (AppContext*)appstate;
-    if (app) {
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+    auto *app = (AppContext *)appstate;
+    if (app)
+    {
         SDL_DestroyRenderer(app->renderer);
         SDL_DestroyWindow(app->window);
-        
+
         // prevent the music from abruptly ending.
         MIX_StopTrack(app->track, MIX_TrackMSToFrames(app->track, 1000));
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        //Mix_FreeMusic(app->music); // this call blocks until the music has finished fading
+        // Mix_FreeMusic(app->music); // this call blocks until the music has finished fading
         SDL_CloseAudioDevice(app->audioDevice);
 
         delete app;
